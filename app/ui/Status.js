dec32e3a44fcdd37c2f5a7076527e2cb05413ff7
Connor Minton <connor.c.minton@gmail.com>
2024-01-26 04:11:59 -0500

    add score animation

--- app/ui/Status.js
+++ app/ui/Status.js
@@ -2,13 +2,26 @@
 
 import styles from './Status.module.css';
 
+import { useState } from 'react';
+
 export default function Status({score, highScore, isGameOver}) {
+  const [ displayScore, setDisplayScore ] = useState(score);
+
+  if (displayScore > score) {
+    setDisplayScore(score);
+  }
+  else if (score != displayScore) {
+    setTimeout(() => {
+      setDisplayScore(displayScore + 1);
+    }, 16 + 500 * Math.exp(-0.5*Math.abs(displayScore-score)));
+  }
+
   const gameOverDisplay = (isGameOver) ? 'block' : 'none';
   return (
     <>
       <p style={{display: gameOverDisplay}} className={styles.status}>{isGameOver ? 'Game Over' : <span>&nbsp;</span>}</p>
       <p className={styles.highScore}>High Score: {highScore}</p>
-      <p className={styles.score}>{score}</p>
+      <p className={styles.score}>{displayScore}</p>
     </>
   );
 };
