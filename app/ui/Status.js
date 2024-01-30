b726ec6c2a255247592d8915de32b940082f5a29
Connor Minton <connor.c.minton@gmail.com>
2024-01-30 18:31:39 -0500

    improve score animation

--- app/ui/Status.js
+++ app/ui/Status.js
@@ -2,18 +2,30 @@
 
 import styles from './Status.module.css';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Status({score, highScore, isGameOver, glowHighScore}) {
   const [ displayScore, setDisplayScore ] = useState(score);
 
+  useEffect(() => {
+    if (score > displayScore) {
+      // This code animates the score counter so that the number counts up rapidly
+      setTimeout(() => {
+        // Increase displayScore by more if score is farther from displayScore.
+        let newDisplayScore = Math.floor(displayScore + Math.pow(Math.abs(displayScore - score), 0.20));
+        if ((newDisplayScore - displayScore) % 10 === 5 || (newDisplayScore - displayScore) % 10 === 0)
+          newDisplayScore--;
+        else if (newDisplayScore === displayScore)
+          newDisplayScore++; // always increase by at least one
+        setDisplayScore(newDisplayScore);
+      },
+        // increase the setTimeout duration (slow the animation) as the displayScore gets closer to score
+        16 + 200 * Math.exp(-0.25*Math.abs(displayScore-score)));
+    }
+  });
+
   if (displayScore > score) {
     setDisplayScore(score);
-  }
-  else if (score != displayScore) {
-    setTimeout(() => {
-      setDisplayScore(displayScore + 1);
-    }, 16 + 500 * Math.exp(-0.5*Math.abs(displayScore-score)));
   }
 
   const gameOverDisplay = (isGameOver) ? 'block' : 'none';
